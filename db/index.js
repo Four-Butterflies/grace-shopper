5e02f9796fbfc27adc3b5d3b2dd9a16099e7055a
rgmantle <graig.mantle@gmail.com>
2021-07-14 13:16:54 -0700

    071421Graig Drop and Create Tables completed with one seeded album

--- db/index.js
+++ db/index.js
@@ -1,13 +1,87 @@
 // Connect to DB
 const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
+// I chose vinyldb as our DB name
+const DB_NAME = 'vinyldb'
+const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
 const client = new Client(DB_URL);
 
 // database methods
+async function createAlbums({
+  album_name,
+  artist,
+  year,
+  price,
+  quantity,
+  reorder,
+  img_url
+}) {
+  try {
+    const { rows: [ album ] } = await client.query(`
+      INSERT INTO products(album_name, artist, year, price, quantity, reorder_number, img_url)
+      VALUES($1, $2, $3, $4, $5, $6, $7)
+      ON CONFLICT (album_name) DO NOTHING
+      RETURNING *;
+    `, [album_name, artist, year, price, quantity, reorder, img_url]);
+
+    return album;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createGenres({
+  genre,
+}) {
+  try {
+    const { rows: [ genrename ] } = await client.query(`
+      INSERT INTO genres(genre)
+      VALUES($1)
+      ON CONFLICT (genre) DO NOTHING
+      RETURNING *;
+    `, [genre]);
+
+    return genrename;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createAlbumGenres(productId, genreId) {
+  try {
+    await client.query(`
+      INSERT INTO genre_albums("productId", "genreId")
+      VALUES ($1, $2)
+      `, [productId, genreId]);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createUser({
+  username,
+  password,
+  email
+}) {
+  try {
+    const { rows: [ user ] } = await client.query(`
+      INSERT INTO users(username, password, email)
+      VALUES($1, $2, $3)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *;
+    `, [username, password, email]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
 
 // export
 module.exports = {
   client,
   // db methods
+  createAlbums,
+  createGenres,
+  createAlbumGenres,
+  createUser
 }
--- db/init_db.js
+++ db/init_db.js
@@ -1,17 +1,54 @@
 // code to build and initialize DB goes here
 const {
-  client
+  client,
   // other db methods 
+  createAlbums,
+  createGenres,
+  createAlbumGenres,
+  createUser
 } = require('./index');
 
 async function buildTables() {
   try {
-    client.connect();
-
-    // drop tables in correct order
+    console.log("Building tables...");
+    await client.connect();
 
     // build tables in correct order
 
+    await client.query(`
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS genre_albums;
+    DROP TABLE IF EXISTS genres;
+    DROP TABLE IF EXISTS products;
+
+    CREATE TABLE products (
+      id SERIAL PRIMARY KEY,
+      album_name varchar(255) UNIQUE NOT NULL,
+      artist varchar(255) NOT NULL,
+      year INT,
+      price INT DEFAULT 1999,
+      quantity INT DEFAULT 0,
+      reorder_number INT DEFAULT 0,
+      img_url varchar(255) DEFAULT 'https://cdn4.vectorstock.com/i/thumb-large/82/48/vinyl-record-blank-realistic-vinyl-disc-mockup-on-vector-17128248.jpg'
+    );
+    CREATE TABLE genres (
+      id SERIAL PRIMARY KEY,
+      genre varchar(255)
+    );
+    CREATE TABLE genre_albums (
+      id SERIAL PRIMARY KEY,
+      "productId" INTEGER REFERENCES products(id) ON DELETE CASCADE NOT NULL,
+      "genreId" INTEGER REFERENCES genres(id) ON DELETE CASCADE NOT NULL
+    );
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username varchar(255) UNIQUE NOT NULL,
+      password varchar(255) NOT NULL,
+      email varchar(255) NOT NULL
+    )
+  `);
+
+    console.log("Tables Built!")
   } catch (error) {
     throw error;
   }
@@ -19,7 +56,19 @@ async function buildTables() {
 
 async function populateInitialData() {
   try {
-    // create useful starting data
+    console.log("Starting to create albums...");
+    await createAlbums({
+      album_name: 'The Colour and the Shape',
+      artist: 'Foo Fighters',
+      year: 1997,
+      price: 1999,
+      quantity: 12,
+      reorder: 1,
+      img_url: 'https://upload.wikimedia.org/wikipedia/en/0/0d/FooFighters-TheColourAndTheShape.jpg'
+    });
+
+    console.log("finished creating albums")
+
   } catch (error) {
     throw error;
   }
