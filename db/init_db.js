8104418db11042ca98175515dd7f25d6554e4023
rgmantle <graig.mantle@gmail.com>
2021-07-17 21:12:09 -0700

    071721 Graig Added Reviews and Cart Tables to init_db. Tested and works

--- db/init_db.js
+++ db/init_db.js
@@ -18,6 +18,8 @@ async function buildTables() {
     // build tables in correct order
 
     await client.query(`
+    DROP TABLE IF EXISTS cart;
+    DROP TABLE IF EXISTS reviews;
     DROP TABLE IF EXISTS users;
     DROP TABLE IF EXISTS genre_albums;
     DROP TABLE IF EXISTS genres;
@@ -49,6 +51,20 @@ async function buildTables() {
       username varchar(255) UNIQUE NOT NULL,
       password varchar(255) NOT NULL,
       email varchar(255) NOT NULL
+    );
+    CREATE TABLE reviews (
+      id SERIAL PRIMARY KEY,
+      review TEXT,
+      rating BOOLEAN DEFAULT true,
+      date DATE DEFAULT CURRENT_DATE,
+      "albumId" INTEGER REFERENCES albums(id) ON DELETE CASCADE NOT NULL,
+      "userId" INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL
+    );
+    CREATE TABLE cart (
+      id SERIAL PRIMARY KEY, 
+      quantity INT DEFAULT 0,
+      "albumId" INTEGER REFERENCES albums(id) ON DELETE CASCADE NOT NULL,
+      "userId" INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL
     )
   `);
 
