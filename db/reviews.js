6fb85b5b3f3e5ba50d835e0815380bec6fe8b2de
Andrea van Ginneken <andrea.vanginneken@gmail.com>
2021-07-19 15:11:35 -0700

    071921 Andrea seeded reviews, added reviews.json

---
+++ db/reviews.js
@@ -0,0 +1,32 @@
+const client = require('./client');
+
+async function createReview({
+  review,
+  rating,
+  albumId,
+  userId
+}) {
+  try {
+    const {
+      rows: [row],
+    } = await client.query(
+      `
+        INSERT INTO reviews(review, rating, "albumId", "userId")
+        VALUES($1, $2, $3, $4)
+        RETURNING *;
+      `,
+      [
+        review,
+        rating,
+        albumId,
+        userId
+      ]
+    );
+
+    return row;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { createReview };
