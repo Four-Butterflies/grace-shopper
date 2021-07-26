5d326eb189cfe71da217b1a9e12f2b2d4b55c4d2
Andrea van Ginneken <andrea.vanginneken@gmail.com>
2021-07-26 16:44:25 -0700

    07262021 Andrea Rudimentary NavBar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { App } from './components';
+import { 
+    Home,
+    NavbarComp
+ } from './components';
+
+import {
+    getSomething
+  } from './api';
+
+const App = () => {
+    const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    getSomething()
+      .then(response => {
+        setMessage(response.message);
+      })
+      .catch(error => {
+        setMessage(error.message);
+      });
+  });
+
+  return (
+    <div className="App">
+      <NavbarComp />
+      <Home />
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
