5581bbaab9cce473277acd62a9f2ffc3575df04a
Burk Manson <burkmanson@gmail.com>
2021-07-28 23:20:23 -0700

    Implemented react-router

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,38 +1,67 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../index.css';
 
-import { 
+import {
   Navbar,
-  Nav, 
-  Container, 
+  Nav,
+  Container,
   NavDropdown,
   Form,
   FormControl,
-  Button
-} from 'react-bootstrap'
+  Button,
+} from 'react-bootstrap';
 
 const NavbarComp = () => {
-    return (
-        <Navbar expand="lg" style={{
-          backgroundColor: 'var(--color-primary)',
-        }}>
-        <Container>
-          <Navbar.Brand href="#home">VINYLS</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" >
-            <Nav className="me-auto" >
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Sale</Nav.Link>
-              <Form inline style={{
-                marginLeft: '600px' // this styling needs to be fixed
-              }}>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              </Form>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    )
-}
+  return (
+    <Navbar
+      expand="lg"
+      style={{
+        backgroundColor: 'var(--color-primary)',
+      }}
+    >
+      <Container>
+        <Link
+          to={'/'}
+          style={{
+            textDecoration: 'none',
+            color: 'white',
+            fontWeight: 'bold',
+            margin: '0.5rem',
+          }}
+        >
+          Vinyl 🤘
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Navbar.Text>
+              <Link to={'/'} style={{ marginLeft: '1rem' }}>
+                Home
+              </Link>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Link to={'/albums'} style={{ marginLeft: '1rem' }}>
+                Albums
+              </Link>
+            </Navbar.Text>
+            <Form
+              inline
+              style={{
+                marginLeft: '600px', // this styling needs to be fixed
+              }}
+            >
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+            </Form>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavbarComp;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import { 
-    Home,
-    NavbarComp
- } from './components';
+import { Home, NavbarComp } from './components';
 
-import {
-    getSomething
-  } from './api';
+import { getSomething } from './api';
 
 const App = () => {
-    const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     getSomething()
-      .then(response => {
+      .then((response) => {
         setMessage(response.message);
       })
-      .catch(error => {
+      .catch((error) => {
         setMessage(error.message);
       });
   });
 
   return (
-    <div className="App">
+    <BrowserRouter>
       <NavbarComp />
-      <Home />
-    </div>
+      <Switch>
+        <Route path={'/albums'}>
+          <div>HELLO! ALBUMS!</div>
+        </Route>
+        <Route path={'/'} exact>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
