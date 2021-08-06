fbc0c28a5600d715fd6b972fcd98d7f5ff8d6a11
rgmantle <graig.mantle@gmail.com>
2021-08-06 12:03:01 -0700

    080621 Added SingleAlbum design to album page, added pages for Contact and About

--- src/components/index.js
+++ src/components/index.js
@@ -10,3 +10,5 @@ export { default as FooterUnit } from './Footer.js';
 export { default as Admin } from './Admin.js';
 export { default as Orders } from './Orders.js';
 export { default as OrdersAlbums } from './OrdersAlbums.js';
+export { default as AboutUs } from './AboutUs.js';
+export { default as Contact } from './Contact.js';
