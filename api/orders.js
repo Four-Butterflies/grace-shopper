fc3309ebd7bc499b898db111402c263553316f7a
Esteban Ordonez <estebanusco@gmail.com>
2021-08-12 21:27:40 -0700

    081221 Esteban Patching api/orders.js holes found by Instructor Eliot. Auth in place and working.

--- api/orders.js
+++ api/orders.js
@@ -25,6 +25,7 @@ ordersRouter.get('/', async (req, res) => {
 });
 
 ordersRouter.get('/details/:orderId', async (req, res) => {
+
   if (Object.keys(req.params).length === 0) {
     return res.status(400).send({
       name: 'InformationRequired',
@@ -35,21 +36,21 @@ ordersRouter.get('/details/:orderId', async (req, res) => {
   const { orderId } = req.params;
 
   try {
+  
     const order = await getOrderWithDetailsByOrderId(orderId);
-    if (!order || order.length === 0) {
-      res.status(500).send({
-        name: 'Orders Not Found',
-        message: 'There are not orders under this user Id.',
-      });
-    }
 
-    res.send(order);
+    !order || order.length === 0 && res.status(500).send({ name: 'Orders Not Found', message: 'There are not orders under this user Id.', })  
+
+    req.user.id !== order[0].userId ? res.status(403).send({name:'Wrong user', message: 'This order belongs to someone else.' }) : res.send(order);
+
+
   } catch (error) {
     console.log(error);
   }
 });
 
 ordersRouter.get('/user_orders/:userId', async (req, res) => {
+console.log(req.user)
   if (Object.keys(req.params).length === 0) {
     return res.status(400).send({
       name: 'InformationRequired',
@@ -60,15 +61,13 @@ ordersRouter.get('/user_orders/:userId', async (req, res) => {
   const { userId } = req.params;
 
   try {
-    const orders = await getOrdersWithDetailsByUserId(userId);
-    if (!orders || orders.length === 0) {
-      res.status(500).send({
-        name: 'Orders Not Found',
-        message: 'There are not orders under this user Id.',
-      });
-    }
 
-    res.send(orders);
+    let orders = await getOrdersWithDetailsByUserId(userId);
+
+    orders = orders.filter(order => req.user.id === order.userId )
+    
+    !orders || orders.length === 0 ? res.status(500).send({ name: 'Orders Not Found',  message: 'There are not orders under this user Id.' }) : res.send(orders);
+
   } catch (error) {
     console.log(error);
   }
@@ -76,6 +75,7 @@ ordersRouter.get('/user_orders/:userId', async (req, res) => {
 
 // POST
 ordersRouter.post('/submit_order', async (req, res) => {
+
   if (Object.keys(req.body).length < 3) {
     return res.status(400).send({
       name: 'InformationRequired',
@@ -84,6 +84,8 @@ ordersRouter.post('/submit_order', async (req, res) => {
   }
 
   const { userId, status, total } = req.body;
+
+  if(req.user.id !== userId) return res.status(403).send({name:'Wrong user', message: 'Please login into your account.' }) 
 
   try {
     const order = await createOrder({ userId, status, total });
