3a82126cf9b9a30361c186a3b3fbe5a67d997ca4
Burk Manson <burkmanson@gmail.com>
2021-08-14 01:32:21 -0700

    Got rid of a warning in console 😎

--- src/components/OrdersAlbums.js
+++ src/components/OrdersAlbums.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { getAlbumById, deleteAlbumUnit } from '../api';
+import {
+  getAlbumById,
+  //deleteAlbumUnit
+} from '../api';
 import { Card, ListGroup, ListGroupItem, CloseButton } from 'react-bootstrap';
 
 const OrdersAlbums = ({ albumId, albumQuantity, albumTotalPrice }) => {
@@ -19,32 +22,41 @@ const OrdersAlbums = ({ albumId, albumQuantity, albumTotalPrice }) => {
   return (
     <div>
       {album ? (
-        <Card style={{ 
-          width: 'auto',
-          margin: '1rem'
-          }}>
-          <Card.Header style={{ 
-            display: 'flex',
-            justifyContent: 'space-between'
-          }}>
+        <Card
+          style={{
+            width: 'auto',
+            margin: '1rem',
+          }}
+        >
+          <Card.Header
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
             <h5>{album.album_name}</h5>
-            <CloseButton 
-              // onClick={(event) => {
-              //   event.preventDefault()
-              //   console.log('from the component', album.id)
-              //   deleteAlbumUnit(album.id)
-              // }}
+            <CloseButton
+            // onClick={(event) => {
+            //   event.preventDefault()
+            //   console.log('from the component', album.id)
+            //   deleteAlbumUnit(album.id)
+            // }}
             />
           </Card.Header>
-          <div style={{ display: 'flex'}}>
-            <Card.Img variant="top" src={album.img_url} alt={album.album_name} style={{ width: '11rem' }}/>
+          <div style={{ display: 'flex' }}>
+            <Card.Img
+              variant="top"
+              src={album.img_url}
+              alt={album.album_name}
+              style={{ width: '11rem' }}
+            />
             <div style={{ width: '100%' }}>
               <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">
                   {album.artist} ({album.year})
                 </Card.Subtitle>
               </Card.Body>
-              <ListGroup className="list-group-flush" >
+              <ListGroup className="list-group-flush">
                 <ListGroupItem>Quantity: {albumQuantity}</ListGroupItem>
                 <ListGroupItem>{album.total_tracks} tracks</ListGroupItem>
                 <ListGroupItem>Price: ${albumTotalPrice / 100}</ListGroupItem>
