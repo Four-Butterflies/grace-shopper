7d7586820d72d79836674dd3d5bea5f8f7af99f4
Burk Manson <burkmanson@gmail.com>
2021-08-14 03:08:58 -0700

    Made Carousel more dynamic and Albums more standard

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
 
-import { PaginationComponent } from '.';
+import { PaginationComponent, SingleAlbum } from '.';
 
-const Albums = ({ allAlbums, setCurrentAlbum }) => {
+const Albums = ({ allAlbums }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
   const totalAlbums = allAlbums.length;
@@ -16,8 +14,6 @@ const Albums = ({ allAlbums, setCurrentAlbum }) => {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  const history = useHistory();
-
   return (
     <>
       <div
@@ -26,58 +22,11 @@ const Albums = ({ allAlbums, setCurrentAlbum }) => {
           flexWrap: 'wrap',
           maxWidth: '100%',
           justifyContent: 'space-around',
-          padding: '1rem'
+          padding: '1rem',
         }}
       >
         {currentAlbums.map((album) => {
-          return (
-            <Card
-              key={album.id}
-              style={{ width: '18rem', marginBottom: '1rem' }}
-            >
-              <Card.Img
-                variant="top"
-                src={album.img_url}
-                alt={album.album_name}
-                style={{
-                  boxShadow:
-                    '0 1px 3px rgba(0, 0, 0, 0.08), 4px 4px 12px #186AE7',
-                  border: '2px solid lightgray',
-                }}
-              />
-              <Card.Body>
-                <Card.Title>{album.album_name}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                  {album.artist} ({album.year})
-                </Card.Subtitle>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroupItem>{album.total_tracks} Tracks</ListGroupItem>
-                <ListGroupItem>Price: ${album.price / 100}</ListGroupItem>
-                <ListGroupItem
-                  style={{ display: 'flex', justifyContent: 'center' }}
-                >
-                  <Button
-                    variant="primary"
-                    style={{
-                      width: '14rem',
-                      fontSize: '0.8rem',
-                      boxShadow:
-                        '0 1px 3px rgba(0, 0, 0, 0.08), 4px 4px 8px #186AE7',
-                      margin: '5px',
-                      height: '2rem',
-                    }}
-                    onClick={async () => {
-                      await setCurrentAlbum(album);
-                      history.push(`/albums/${album.id}`);
-                    }}
-                  >
-                    See Details
-                  </Button>
-                </ListGroupItem>
-              </ListGroup>
-            </Card>
-          );
+          return <SingleAlbum key={album.id} album={album} />;
         })}
       </div>
       <span style={{ display: 'flex', justifyContent: 'center' }}>
